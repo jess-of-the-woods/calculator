7e4846501cae9936d1eaaf4c0c0146fc3b968fa9
jess-of-the-woods <what.the.dickenss@gmail.com>
2016-03-02 12:49:49 +1300

    add functions to input into results, new styles

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,39 @@
 var run1 = function(){
   document.calc.result.value = "1";
 }
+var run2 = function(){
+  document.calc.result.value = "2";
+}
+var run3 = function(){
+  document.calc.result.value = "3";
+}
+var run4 = function(){
+  document.calc.result.value = "4";
+}
+var run5 = function(){
+  document.calc.result.value = "5";
+}
+var run6 = function(){
+  document.calc.result.value = "6";
+}
+var run7 = function(){
+  document.calc.result.value = "7";
+}
+var run8 = function(){
+  document.calc.result.value = "8";
+}
+var run9 = function(){
+  document.calc.result.value = "9";
+}
+var runTimes = function(){
+  document.calc.result.value = "*";
+}
+var runDivide = function(){
+  document.calc.result.value = "/";
+}
+var runAdd = function(){
+  document.calc.result.value = "+";
+}
+var runSubtract = function(){
+  document.calc.result.value = "-";
+}
