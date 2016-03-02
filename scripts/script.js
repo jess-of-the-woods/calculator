cb53873415874d84254c143fbc0be5f57d456ca8
LizzieLaShark <lizzie.s.sullivan@gmail.com>
2016-03-02 14:19:33 +1300

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,61 @@
 
 var run1 = function(){
-  document.calc.result.value = "1";
+  document.calc.result.value += "1";
 }
 var run2 = function(){
-  document.calc.result.value = "2";
+  document.calc.result.value += "2";
 }
 var run3 = function(){
-  document.calc.result.value = "3";
+  document.calc.result.value += "3";
 }
 var run4 = function(){
-  document.calc.result.value = "4";
+  document.calc.result.value += "4";
 }
 var run5 = function(){
-  document.calc.result.value = "5";
+  document.calc.result.value += "5";
 }
 var run6 = function(){
-  document.calc.result.value = "6";
+  document.calc.result.value += "6";
 }
 var run7 = function(){
-  document.calc.result.value = "7";
+  document.calc.result.value += "7";
 }
 var run8 = function(){
-  document.calc.result.value = "8";
+  document.calc.result.value += "8";
 }
 var run9 = function(){
-  document.calc.result.value = "9";
+  document.calc.result.value += "9";
 }
 var runTimes = function(){
-  document.calc.result.value = "*";
+  document.calc.result.value += "*";
 }
 var runDivide = function(){
-  document.calc.result.value = "/";
+  document.calc.result.value += "/";
 }
 var runAdd = function(){
-  document.calc.result.value = "+";
+  document.calc.result.value += "+";
 }
 var runSubtract = function(){
-  document.calc.result.value = "-";
+  document.calc.result.value += "-";
 }
+
+// var runEquals = function() {
+//   document.calc.result.value.innerHTML = eval(document.calc.result.value);
+//   console.log(eval());
+// }
+
+var evaluate = function() {
+	var evalu = eval(document.calc.result.value);
+	document.calc.result.value = evalu;
+}
+
+
+bla bla bla
+
+
+
+
+
+
+
+
